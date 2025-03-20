07fb5ddc5dc2896b40276b71ebd7171ad7f88151
TuNghia-leonix <nghiant@leonix.vn>
2025-03-20 10:28:46 +0700

    fix no altribuilt

--- __manifest__.py
+++ __manifest__.py
@@ -1,33 +1,19 @@
-# -*- coding: utf-8 -*-
 {
     'name': "Leonix Custom VietNam State Module",
-
     'summary': """
         Custom data VN State by Leonix""",
-
     'description': """
         This module provides custom functionalities developed by Leonix to enhance Odoo features""",
-
     'author': "Leonix",
     'website': "http://leonix.vn",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Uncategorized',
-    'version': '18.0',
-
-    # any module necessary for this one to work correctly
+    'version': '18.0.18.0',
     'depends': ['base'],
-
-    # always loaded
+    'installable': True,
+    'auto_install': False,
+    'application': False,
     'data': [
-        'data/update_vn_states.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        '',
-    ],
+    'data/cron.xml',
+],
     'license': 'OPL-1',
-    'post_init_hook': 'create_vn_states',
 }
--- models/res_country_state.py
+++ models/res_country_state.py
@@ -1,47 +1,55 @@
-from odoo import api, SUPERUSER_ID
-def create_vn_states(env):
-    vietnam = env['res.country'].search([('code', '=', 'VN')], limit=1)
-    if vietnam:
-        env['res.country.state'].search([('country_id', '=', vietnam.id)]).unlink()
-        states = [
-            {'name': 'Hà Nội', 'code': 'HN'},
-            {'name': 'Hồ Chí Minh', 'code': 'HCM'},
-            {'name': 'Đà Nẵng', 'code': 'DN'},
-            {'name': 'Hải Phòng', 'code': 'HP'},
-            {'name': 'Cần Thơ', 'code': 'CT'},
-            {'name': 'Huế', 'code': 'H'},
-            {'name': 'Tuyên Quang', 'code': 'VN-01'},
-            {'name': 'Lào Cai', 'code': 'VN-02'},
-            {'name': 'Thái Nguyên', 'code': 'VN-03'},
-            {'name': 'Phú Thọ', 'code': 'VN-04'},
-            {'name': 'Bắc Ninh', 'code': 'VN-05'},
-            {'name': 'Hưng Yên', 'code': 'VN-06'},
-            {'name': 'Lạng Sơn', 'code': 'VN-07'},
-            {'name': 'Ninh Bình', 'code': 'VN-08'},
-            {'name': 'Điện Biên', 'code': 'VN-09'},
-            {'name': 'Lai Châu', 'code': 'VN-10'},
-            {'name': 'Sơn La', 'code': 'VN-11'},
-            {'name': 'Cao Bằng', 'code': 'VN-12'},
-            {'name': 'Quảng Ninh', 'code': 'VN-13'},
-            {'name': 'Thanh Hóa', 'code': 'VN-14'},
-            {'name': 'Nghệ An', 'code': 'VN-15'},
-            {'name': 'Hà Tĩnh', 'code': 'VN-16'},
-            {'name': 'Quảng Trị', 'code': 'VN-17'},
-            {'name': 'Quảng Ngãi', 'code': 'VN-18'},
-            {'name': 'Gia Lai', 'code': 'VN-19'},
-            {'name': 'Đắc Lắk', 'code': 'VN-20'},
-            {'name': 'Khánh Hòa', 'code': 'VN-21'},
-            {'name': 'Lâm Đồng', 'code': 'VN-22'},
-            {'name': 'Đồng Nai', 'code': 'VN-23'},
-            {'name': 'Tây Ninh', 'code': 'VN-24'},
-            {'name': 'Đồng Tháp', 'code': 'VN-25'},
-            {'name': 'An Giang', 'code': 'VN-26'},
-            {'name': 'Vĩnh Long', 'code': 'VN-27'},
-            {'name': 'Cà Mau', 'code': 'VN-28'},          
-        ]
-    for state in states:
-        env['res.country.state'].create({
-            'name': state['name'],
-            'code': state['code'],
-            'country_id': vietnam.id,
-        })
+from odoo import api, models, SUPERUSER_ID
+
+class UpdateVNState(models.Model):
+    _inherit = "res.country.state"
+
+    @api.model
+    def create_vn_states(self):
+        """Xóa và tạo lại danh sách tỉnh/thành Việt Nam"""
+        vietnam = self.env['res.country'].search([('code', '=', 'VN')], limit=1)
+        if vietnam:
+            self.search([('country_id', '=', vietnam.id)]).unlink()  # Xóa các tỉnh cũ
+
+            states = [
+                {'name': 'Hà Nội', 'code': 'HN'},
+                {'name': 'Hồ Chí Minh', 'code': 'HCM'},
+                {'name': 'Đà Nẵng', 'code': 'DN'},
+                {'name': 'Hải Phòng', 'code': 'HP'},
+                {'name': 'Cần Thơ', 'code': 'CT'},
+                {'name': 'Huế', 'code': 'H'},
+                {'name': 'Tuyên Quang', 'code': 'VN-01'},
+                {'name': 'Lào Cai', 'code': 'VN-02'},
+                {'name': 'Thái Nguyên', 'code': 'VN-03'},
+                {'name': 'Phú Thọ', 'code': 'VN-04'},
+                {'name': 'Bắc Ninh', 'code': 'VN-05'},
+                {'name': 'Hưng Yên', 'code': 'VN-06'},
+                {'name': 'Lạng Sơn', 'code': 'VN-07'},
+                {'name': 'Ninh Bình', 'code': 'VN-08'},
+                {'name': 'Điện Biên', 'code': 'VN-09'},
+                {'name': 'Lai Châu', 'code': 'VN-10'},
+                {'name': 'Sơn La', 'code': 'VN-11'},
+                {'name': 'Cao Bằng', 'code': 'VN-12'},
+                {'name': 'Quảng Ninh', 'code': 'VN-13'},
+                {'name': 'Thanh Hóa', 'code': 'VN-14'},
+                {'name': 'Nghệ An', 'code': 'VN-15'},
+                {'name': 'Hà Tĩnh', 'code': 'VN-16'},
+                {'name': 'Quảng Trị', 'code': 'VN-17'},
+                {'name': 'Quảng Ngãi', 'code': 'VN-18'},
+                {'name': 'Gia Lai', 'code': 'VN-19'},
+                {'name': 'Đắc Lắk', 'code': 'VN-20'},
+                {'name': 'Khánh Hòa', 'code': 'VN-21'},
+                {'name': 'Lâm Đồng', 'code': 'VN-22'},
+                {'name': 'Đồng Nai', 'code': 'VN-23'},
+                {'name': 'Tây Ninh', 'code': 'VN-24'},
+                {'name': 'Đồng Tháp', 'code': 'VN-25'},
+                {'name': 'An Giang', 'code': 'VN-26'},
+                {'name': 'Vĩnh Long', 'code': 'VN-27'},
+                {'name': 'Cà Mau', 'code': 'VN-28'},          
+            ]
+
+            for state in states:
+                self.create({
+                    'name': state['name'],
+                    'code': state['code'],
+                    'country_id': vietnam.id,
+                })
